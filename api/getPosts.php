65524541fb4d9195ca36aacbb7d8e96e851340ba
cloud-techno <ferdiates157@gmail.com>
2025-12-06 12:04:26 +0100

    db

---
+++ api/getPosts.php
@@ -0,0 +1,4 @@
+<?php
+$config = require $_SERVER['DOCUMENT_ROOT'] . '/../secure_config/config.php';
+
+echo "DB connected!";
---
+++ config/config.example.php
@@ -0,0 +1,8 @@
+<?php
+// Example config — GitHub'da durur
+return [
+    'host' => 'DB_HOST',
+    'username' => 'DB_USER',
+    'password' => 'DB_PASS',
+    'database' => 'DB_NAME'
+];
