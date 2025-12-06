7f246d3ec635ae39aa29527e2a9283fbc00aedbe
cloud-techno <ferdiates157@gmail.com>
2025-12-06 12:20:24 +0100

    dd

--- api/getPosts.php
+++ api/getPosts.php
@@ -1,4 +1,21 @@
 <?php
 $config = require $_SERVER['DOCUMENT_ROOT'] . '/../secure_config/config.php';
 
-echo "DB connected!";
+$conn = new mysqli(
+    $config['host'],
+    $config['username'],
+    $config['password'],
+    $config['database']
+);
+
+header('Content-Type: application/json');
+
+$result = $conn->query("SELECT * FROM posts ORDER BY date DESC");
+
+$posts = [];
+
+while($row = $result->fetch_assoc()) {
+    $posts[] = $row;
+}
+
+echo json_encode($posts);
