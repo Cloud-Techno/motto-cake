f7684db0134ac03164952cabcb13309716962917
Cloud-Techno <ferdiates157@gmail.com>
2025-12-10 21:53:51 +0100

    afa

--- api/getPosts.php
+++ api/getPosts.php
@@ -3,8 +3,8 @@ header('Content-Type: application/json');
 include $_SERVER['DOCUMENT_ROOT'].'/../secure_config/config.php';
 $conn->set_charset("utf8mb4");
 
-// Verileri çek
-$sql = "SELECT title, content, created_at, image_url FROM posts ORDER BY created_at DESC";
+
+$sql = "SELECT id, title, content, created_at, image_url FROM posts ORDER BY created_at DESC";
 $result = $conn->query($sql);
 
 $posts = [];
@@ -12,15 +12,15 @@ $posts = [];
 if ($result->num_rows > 0) {
     while ($row = $result->fetch_assoc()) {
         $posts[] = [
+            "id" => $row['id'],
             "title" => $row['title'],
             "content" => $row['content'],
             "created_at" => $row['created_at'],
-            "image_url" => $row['image_url']   
+            "image_url" => $row['image_url']
         ];
     }
 }
 
 echo json_encode($posts, JSON_UNESCAPED_UNICODE);
-
 $conn->close();
 ?>
